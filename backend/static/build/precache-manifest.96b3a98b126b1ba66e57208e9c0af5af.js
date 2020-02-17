814968cda8cb08b0d645d1de479fe230d76c0627
ftlll <857785996@qq.com>
2020-02-17 11:45:28 -0500

    create frontend and backend

---
+++ backend/static/build/precache-manifest.96b3a98b126b1ba66e57208e9c0af5af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc65a9b6a59ef4a21cf4cb79e13e7b3d",
+    "url": "/backend/static/build/index.html"
+  },
+  {
+    "revision": "313647c97192285eec97",
+    "url": "/backend/static/build/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "189f8f0c7eebc06bfcae",
+    "url": "/backend/static/build/static/js/2.0dcfeaca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/backend/static/build/static/js/2.0dcfeaca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313647c97192285eec97",
+    "url": "/backend/static/build/static/js/main.fd7dedad.chunk.js"
+  },
+  {
+    "revision": "197478eaa0bf2f328717",
+    "url": "/backend/static/build/static/js/runtime-main.2e80534e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/backend/static/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Upload from './Components/Upload';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>File Upload</h1>
+      <Upload />
+    </div>
+  );
+}
+
+export default App;
