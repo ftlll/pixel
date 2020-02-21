d768f5a391bf7ed13eccd703fedf8eabb77ea3c3
ftlll <857785996@qq.com>
2020-02-20 21:25:35 -0500

    add color picker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './css/imports.css';
 
 import Cell from './components/Cell';
+import ColorPicker from './components/ColorPicker';
 
 function App() {
   return (
     <div className='background'>
       <div className='header'>222</div>
       <Cell />
+      <ColorPicker />
     </div>
   );
 }
