cae700742bec1432d77c8d57afbb8fcdf7c1e71e
Tianlu Feng <tfeng@opentext.com>
2020-02-21 13:19:31 -0500

    add row and column in canvas to display cells

--- frontend/src/components/Cell.jsx
+++ frontend/src/components/Cell.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const CELL_NULL_COLOR = 'rgba(49, 49, 49, 1)';
-const WIDTH = 10;
 
 class Cell extends React.Component {
     
     render() {
         const {cell: {color, width}} = this.props;
         const styles = {
-            width: `${width}px`,
-            height: `${width}px`,
-            margin: `${width/10}px`,
+            width: `${width}%`,
+            height: `${width}%`,
+            margin: `${width/20}%`,
             backgroundColor:  color || CELL_NULL_COLOR
         };
 
--- frontend/src/components/Grid.jsx
+++ frontend/src/components/Grid.jsx
@@ -4,16 +4,21 @@ import Cell from './Cell';
 class Grid extends React.Component {
     
     render() {
+        const {style, row, column} = this.props;
         const cell = {
             color: '#3333ff',
-            width: 10
+            width: 100/row
+        };
+        const cell2 = {
+            width: 100/row
         };
         const cells = [];
         for(let i = 0; i < 10; i++) {
             cells.push(cell);
+            cells.push(cell2);
         }
         return (
-            <div>
+            <div style={style}>
                 {cells.map(cell => {
                     return (<Cell cell={cell}/>)
                 })}
