18c9da302462ac5421a44a5ca6990f2ce6024d11
ftlll <857785996@qq.com>
2020-02-23 12:03:27 -0500

    add redux dev tool and set init state action

--- frontend/src/redux/actions/action.js
+++ frontend/src/redux/actions/action.js
@@ -1,6 +1,12 @@
 import * as type from './actionTypes';
 import { ActionCreators } from 'redux-undo';
 
+export function init() {
+    return {
+        type: type.SET_INIT_STATE
+    }
+}
+
 export function setColor (color) {
     return {
         type: type.SET_COLOR,
--- frontend/src/redux/actions/actionTypes.js
+++ frontend/src/redux/actions/actionTypes.js
@@ -1,2 +1,3 @@
 export const SET_COLOR = 'SET_COLOR';
+export const SET_INIT_STATE = 'SET_INIT_STATE'; 
 
