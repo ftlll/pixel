419675100824eb8a66daac39d1e6e71596c00216
ftlll <857785996@qq.com>
2020-03-01 22:40:45 -0500

    remove console.log

--- frontend/src/components/Eraser.jsx
+++ frontend/src/components/Eraser.jsx
@@ -19,10 +19,7 @@ const mapStateToProps = state => ({
 });
   
 const mapDispatchToProps = dispatch => ({
-    switchEraser: () => {
-        dispatch(switchTool(tool.ERASER));
-        console.log(store.getState());
-    }
+    switchEraser: () => dispatch(switchTool(tool.ERASER))
 });
 
 const EraserContainer = connect(mapStateToProps, mapDispatchToProps)(Eraser);
