5820b7c2b056059eafb83bbda1db48ceb6428c41
Tianlu Feng <tfeng@opentext.com>
2020-03-11 17:30:00 -0400

    add basic functions for popup

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import Modal from 'react-awesome-modal';
 
 class Popup extends React.Component {
-    
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         visible : false
+    //     }
+    // }
+    render() {
+        return (
+            <Modal 
+                visible={this.props.visible}
+                
+                effect="fadeInUp"
+                onClickAway={() => this.props.close()}>
+                <div>
+                    <h1>Title</h1>
+                    <p>Some Contents</p>
+                    <a onClick={() => this.props.close()}>Close</a>
+                </div>
+            </Modal>
+        );
+    }
 }
 
 export default Popup;
