4bf443230b983481aa2a9fe4bdb6dab99d5bcc99
ftlll <857785996@qq.com>
2020-03-15 14:45:24 -0400

    modify css of modal

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import Modal from 'react-awesome-modal';
 
 class Popup extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         visible : false
-    //     }
-    // }
     render() {
         return (
             <Modal 
                 visible={this.props.visible}
-                
+                width="80%"
+                height="80%"
                 effect="fadeInUp"
                 onClickAway={() => this.props.close()}>
                 <div>
                     <h1>Title</h1>
+                    <h1>Title</h1><h1>Title</h1><h1>Title</h1><h1>Title</h1>
                     <p>Some Contents</p>
                     <a onClick={() => this.props.close()}>Close</a>
                 </div>
