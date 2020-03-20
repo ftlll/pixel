3b71d73f89b83b3c87c6826ff2ad33bf577e6121
ftlll <857785996@qq.com>
2020-03-19 22:47:42 -0400

    add api for upload file

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask,render_template,g,session
 from flask_cors import CORS
 
-# def creat_app():
-#     app = Flask(__name__,template_folder="templates",static_folder="static",static_url_path="/backend/static")
-#     CORS(app)
-#     from . import main
-#     app.register_blueprint(main.main)
-#     app.config['SECRET_KEY'] = ''
-#     app.debug = True
-#     # db.init_app(app)
-#     return app
-
-app = creat_app()
+def creat_app():
+    app = Flask(__name__,template_folder="templates",static_folder="static",static_url_path="/backend/static")
+    CORS(app)
+    from . import main
+    app.register_blueprint(main.main)
+    app.config['SECRET_KEY'] = ''
+    app.debug = True
+    # db.init_app(app)
+    return app
+    
