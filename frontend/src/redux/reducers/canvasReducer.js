97744a8043c094689d62e8d43607d1aae0dfec43
ftlll <857785996@qq.com>
2020-03-29 03:54:27 -0400

    modify change dimension function for multiple canvas

--- frontend/src/redux/reducers/canvasReducer.js
+++ frontend/src/redux/reducers/canvasReducer.js
@@ -39,43 +39,51 @@ const importPixelate = (action) => {
   })
 }
 
-const changeDimension = (canvas, action) => {
-  const grid = canvas.get('grid');
-  const columns = action.columns;
-  const rows = action.rows;
-  const newColumns = action.newColumns;
-  const newRows = action.newRows;
+const changeDimensionForOne = (grid, columns, rows, newColumns, newRows) => {
   const dRow = rows - newRows;
   const dCol = columns - newColumns;
   let newGrid = grid;
   if (dRow > 0) {
     for (let i = 0; i < columns; i++) {
-      for (let j = 0; i < dRow; i++) {
+      for (let j = 0; j < dRow; j++) {
         newGrid = newGrid.push('');
       }
     }
   } else if (dRow < 0) {
     for (let i = 0; i < columns; i++) {
-      for (let j = 0; i < dRow; i++) {
+      for (let j = 0; j < dRow; j++) {
         newGrid = newGrid.splice(-1, 1);
       }
     }
   } else if (dCol > 0) {
     for (let i = columns * rows; i > 0; i -= columns) {
-      for (let j = 0; i < dCol; i++) {
+      for (let j = 0; j < dCol; j++) {
         newGrid = newGrid.insert(i, '');
       }
     }
   } else if (dCol < 0) {
     for (let i = columns * rows; i > 0; i -= columns) {
-      for (let j = 0; i < dCol; i++) {
+      for (let j = 0; j < dCol; j++) {
         newGrid = newGrid.splice(i - 1, 1);
       }
     }
-  } else {
-    return grid;
   }
+  return newGrid;
 }
+
+const changeDimension = (canvas, action) => {
+  const grid = canvas.get('grid');
+  const columns = canvas.get('columns');
+  const rows = canvas.get('rows');
+  const newColumns = action.newColumns;
+  const newRows = action.newRows;
+  const newGrid = changeDimensionForOne(grid, columns, rows, newColumns, newRows);
+  return canvas.merge({
+    columns: newColumns,
+    rows: newRows,
+    grid: newGrid
+  });
+};
 
 export default function(canvas = initCanvas(), action) {
     switch (action.type) {
